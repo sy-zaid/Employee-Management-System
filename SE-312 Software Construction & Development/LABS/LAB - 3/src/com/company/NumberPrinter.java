f63624c943653f6d5132344cee3a2158fc8b8a3a
SyedZaid <syedmzaid.99@gmail.com>
2024-03-25 00:46:44 +0500

    starting AI lab 4

---
+++ SE-312 Software Construction & Development/LABS/LAB - 3/src/com/company/NumberPrinter.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class NumberPrinter {
+    public static void printNumbers(){
+        int num1 = 10;
+        int num2 = 20;
+        double numPI = 3.14;
+        System.out.println("Printing Numbers:");
+        System.out.println("Number 1" + num1);
+        System.out.println("Number 2: " + num2);
+        System.out.println("Number 3: " + numPI);
+    };
+};
